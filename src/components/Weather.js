6f512700a21b0d632f90af6937723d481f7ef762
Kaan Tetik <kaan.tk28@gmail.com>
2023-07-26 23:31:56 +0300

    Rendering of incoming data has been completed.

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -8,18 +8,51 @@ function Weather() {
   const getWeatherImage = (weatherCondition) => {
     switch (weatherCondition) {
       case 'Clear':
-        console.log(weatherData.weather[0].main)
-        return '../images/clear.png';
-    }
+        return '/img/clear.png';
+      case 'Rain':
+        return '/img/rain.png';
+      case 'Snow':
+        return '/img/snow.png';
+      case 'Clouds':
+        return '/img/cloud.png';
+      case 'Haze':
+        return '/img/mist.png';
+      }
   }
 
   return (
     <div className="weather-result">
-      <code>{}</code>
       {loading && <span>Loading...</span>}
 
-      {weatherData && weatherData.weather && weatherData.weather.length > 0 &&
-      <img src={getWeatherImage(weatherData.weather[0].main)} alt="Weather Condition" />}
+      {weatherData && (
+        <>
+          <div className='weather-result'>
+            <img src={getWeatherImage(weatherData.weather[0].main)} alt="Weather Condition"/>
+            <span className='cityName'>{weatherData.name}</span>
+            <span className='temp'>{weatherData.main.temp}°</span>
+          </div>
+          <div className='weather-result-details'>
+            <div className="humidity">
+                <i className="fa-solid fa-water fa-2xl"></i>
+                <div className="text">
+                  <span>{weatherData.main.humidity}</span>
+                  <p>Nem Oranı</p>
+                </div>
+            </div>
+            <div className="wind">
+                <i className="fa-solid fa-wind fa-2xl"></i>
+                <div className="text">
+                  <span>{weatherData.wind.speed}</span>
+                  <p>Rüzgar hızı</p>
+                </div>
+            </div>
+          </div>
+        </>
+      )}
+
+      { weatherData && (
+        console.log(JSON.stringify(weatherData))
+      )}
 
     </div>
   )
